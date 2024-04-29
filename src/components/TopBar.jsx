bf2433553eba3f0db5ff16ddb9ddb73339c47b3f
Takács Martin <originalmartin97@gmail.com>
2024-04-29 18:29:24 +0200

    TopBar is up to date

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,41 +1,58 @@
-import * as React from 'react';
+// Importing necessary libraries and components
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Button from '@mui/material/Button'
-import { signOut } from 'firebase/auth'
-import { auth } from '../firebase'
+import Button from '@mui/material/Button';
+import Hidden from '@mui/material/Hidden';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
 
+// TopBar component
 export const TopBar = ({ sideBarWidth, toggleSideBar }) => {
+  // Function to handle sign out
+  const handleSignOut = (event) => {
+    event.stopPropagation(); // Prevents the event from bubbling up the DOM tree
+    signOut(auth); // Signs out the user
+  };
+
+  // Returns the TopBar component
   return (
-    <AppBar position="fixed" sx={{
-      backgroundColor: "#2f312F",
-    }}>
+    <AppBar position="fixed" sx={{ backgroundColor: "#2f312F" }}>
       <Toolbar
         sx={{
+          // Adjusts the left margin based on the screen size
           ml: { xs: 0, sm: 0, md: 0, lg: `${sideBarWidth}px`, xl: `${sideBarWidth}px` },
+          display: 'flex', // Makes the Toolbar a flex container
+          justifyContent: 'space-between', // Distributes the space between the Toolbar items evenly
         }}
       >
-        <IconButton
-          sx={{
-            color: "#f7faf7",
-            "&:hover": { backgroundColor: "#a1f5ce", color: "black" },
-          }}
-          onClick={toggleSideBar}
-        >
-          <MenuIcon />
-        </IconButton>
+        {/* Hides the IconButton on large screens and up */}
+        <Hidden lgUp>
+          <IconButton
+            sx={{
+              color: "#f7faf7",
+              // Changes the background color and text color on hover
+              "&:hover": { backgroundColor: "#a1f5ce", color: "black" },
+            }}
+            onClick={toggleSideBar} // Toggles the sidebar when clicked
+          >
+            <MenuIcon /> {/* Menu icon */}
+          </IconButton>
+        </Hidden>
+        {/* Site title */}
         <Typography variant="h6" color='#f7faf7'>
           FAIQnA
         </Typography>
+        {/* Log out button */}
         <Button sx={{
           backgroundColor: "#99bdb1", color: "black",
+          // Changes the background color on hover
           "&:hover": { backgroundColor: "#c57d83" },
-          padding: "1rem",
-        }} onClick={(event) => { event.stopPropagation(); signOut(auth) }}>
+          padding: "1rem", // Adds padding
+        }} onClick={handleSignOut}> {/* Calls the handleSignOut function when clicked */}
           Log out
         </Button>
       </Toolbar>
